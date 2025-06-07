d1b0766c8298310140458a08fd5922733a1712f8
Jeremy Daniel Estrada <jeremydanielestrada2001@gmail.com>
2025-06-07 10:07:15 +0800

    added user controller and routes

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,21 @@
 use App\Http\Controllers\API\CarouselItemsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-
+//Carousel Items Routes
 Route::get('/carousel',[CarouselItemsController::class, 'index']);
 Route::get('/carousel/{id}',[CarouselItemsController::class, 'show']);
 Route::post('/carousel',[CarouselItemsController::class, 'store']);
 Route::delete('/carousel/{id}',[CarouselItemsController::class, 'destroy']);
+
+//User Routes
+Route::get('/user',[UserController::class, 'index']);
+Route::delete('/user/{id}',[UserController::class, 'destroy']);
 
 
